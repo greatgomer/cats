684f7f692383a6b0d28e91af0ab474937dbdbdc4
Victor Spirin <greatgomer2@gmail.com>
2020-08-11 15:26:16 +0300

    Add simple tests

--- app/src/androidTest/java/com/example/cats/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cats/ExampleInstrumentedTest.java
@@ -1,14 +1,26 @@
 package com.example.cats;
 
-import android.content.Context;
+import androidx.test.runner.AndroidJUnit4;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.example.cats.api.models.res.Cat;
+import com.example.cats.api.services.ImagesService;
+import com.example.cats.api.services.NetworkProvider;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import retrofit2.Call;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Instrumented image_background, which will execute on an Android device.
@@ -17,10 +29,32 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under image_background.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.cats", appContext.getPackageName());
+    public static HashMap<String, String> params = new HashMap<>();
+
+    URL api = new URL("https://api.thecatapi.com/v1/images/search");
+    HttpsURLConnection myConnection =
+            (HttpsURLConnection) api.openConnection();
+
+    NetworkProvider retrofit = new NetworkProvider();
+    ImagesService service = retrofit.createImagesService();
+    Call<List<Cat>> repos = service.getAllCats(params);
+
+    public ExampleInstrumentedTest() throws IOException {
     }
+
+    @Before
+    public void setUp() {
+        myConnection.setRequestProperty("x-api-key", "97a76886-9a72-4bb1-81a2-e730833ffbdb");
+    }
+
+    @Test
+    public void testCatsConnect() throws IOException {
+        assertEquals(200, myConnection.getResponseCode());
+    }
+
+    @Test
+    public void testCatsData() {
+        assertNotNull(repos);
+    }
+
 }
