585f13a459765fd85dc59cd2455738f97314c28c
Victor Spirin <greatgomer2@gmail.com>
2020-08-11 16:14:32 +0300

    Add more tests

--- app/src/androidTest/java/com/example/cats/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cats/ExampleInstrumentedTest.java
@@ -2,7 +2,12 @@ package com.example.cats;
 
 import androidx.test.runner.AndroidJUnit4;
 
+import com.example.cats.api.models.req.FavoritesParameters;
 import com.example.cats.api.models.res.Cat;
+import com.example.cats.api.models.res.Downloads;
+import com.example.cats.api.models.res.Favorites;
+import com.example.cats.api.services.DownloadsService;
+import com.example.cats.api.services.FavouritesService;
 import com.example.cats.api.services.ImagesService;
 import com.example.cats.api.services.NetworkProvider;
 
@@ -30,20 +35,29 @@ import static junit.framework.TestCase.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     public static HashMap<String, String> params = new HashMap<>();
-
-    URL api = new URL("https://api.thecatapi.com/v1/images/search");
-    HttpsURLConnection myConnection =
-            (HttpsURLConnection) api.openConnection();
+    HttpsURLConnection myConnection;
+    String sub_id = "test3";
+    URL api;
 
     NetworkProvider retrofit = new NetworkProvider();
-    ImagesService service = retrofit.createImagesService();
-    Call<List<Cat>> repos = service.getAllCats(params);
+    ImagesService imagesService = retrofit.createImagesService();
+    Call<List<Cat>> cats = imagesService.getAllCats(params);
 
-    public ExampleInstrumentedTest() throws IOException {
-    }
+    FavouritesService favouritesService = retrofit.createFavouritesService();
+
+    Call<List<Favorites>> favourites = favouritesService.getAllFavorites(sub_id);
+
+    FavoritesParameters favoritesParameters = new FavoritesParameters("1ud", sub_id);
+    Call<FavoritesParameters> favoritesAdd = favouritesService.postFavourites(favoritesParameters);
+
+    DownloadsService downloadsService = retrofit.createDownloadsService();
+    Call<List<Downloads>> downloads = downloadsService.getAllDownloads(sub_id, 10);
 
     @Before
-    public void setUp() {
+    public void catsConnect() throws IOException {
+        api = new URL("https://api.thecatapi.com/v1/images/search");
+        myConnection =
+                (HttpsURLConnection) api.openConnection();
         myConnection.setRequestProperty("x-api-key", "97a76886-9a72-4bb1-81a2-e730833ffbdb");
     }
 
@@ -52,9 +66,50 @@ public class ExampleInstrumentedTest {
         assertEquals(200, myConnection.getResponseCode());
     }
 
+    @Before
+    public void favouritesConnect() throws IOException {
+        api = new URL("https://api.thecatapi.com/v1/favourites");
+        myConnection =
+                (HttpsURLConnection) api.openConnection();
+        myConnection.setRequestProperty("x-api-key", "97a76886-9a72-4bb1-81a2-e730833ffbdb");
+    }
+
     @Test
-    public void testCatsData() {
-        assertNotNull(repos);
+    public void testFavouritesConnect() throws IOException {
+        assertEquals(200, myConnection.getResponseCode());
+    }
+
+    @Before
+    public void downloadsConnect() throws IOException {
+        api = new URL("https://api.thecatapi.com/v1/downloads");
+        myConnection =
+                (HttpsURLConnection) api.openConnection();
+        myConnection.setRequestProperty("x-api-key", "97a76886-9a72-4bb1-81a2-e730833ffbdb");
+    }
+
+    @Test
+    public void testDownloadsConnect() throws IOException {
+        assertEquals(200, myConnection.getResponseCode());
+    }
+
+    @Test
+    public void testGetAllCatsData() {
+        assertNotNull(cats);
+    }
+
+    @Test
+    public void testGetAllFavouritesData() {
+        assertNotNull(favourites);
+    }
+
+    @Test
+    public void testAddInFavourites(){
+        assertNotNull(favoritesAdd);
+    }
+
+    @Test
+    public void testGetAllDownloadsData() {
+        assertNotNull(downloads);
     }
 
 }
